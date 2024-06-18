25d1a9d3e65a09d9a0288f8ee6b42af9203b750c
WannaMinPaing <paingwanna@gmail.com>
2024-06-18 22:06:27 +0630

    Resolved merge conflicts

---
+++ app.go
@@ -0,0 +1,9 @@
+package main // To know Golang where begin to start
+
+import "fmt"
+
+func main() {
+	// To know Golang where begin to execute and main function has only one in the program and
+	// must not include main function in other file
+	fmt.Print("Hello")
+}
