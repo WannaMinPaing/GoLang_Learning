ee3ca9303ada1dcadfa6fabd8941d66c48815bec
WannaMinPaing <paingwanna@gmail.com>
2024-07-20 21:27:00 +0630

    file read and write

--- investment_calculator.go
+++ investment_calculator.go
@@ -1,15 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func getNumberFromFile() float64 {
+	data, _ := os.ReadFile("number.txt")
+	numberText := string(data)
+	number, _ := strconv.ParseFloat(numberText, 64)
+	//strconv => string to conversition
+	// float 64
+	return number
+}
+
+func writeNumberToFile(number int) {
+	numberText := fmt.Sprint(number)
+	os.WriteFile("number.txt", []byte(numberText), 0644)
+}
 
 func main() {
-
-	for i := 0; i < 4; i++ {
-
-		if i%2 == 0 {
-			fmt.Println("Sum result", i)
-		} else {
-			fmt.Println("Odd result", i)
-		}
-	}
+	writeNumberToFile(2)
+	var getNumber = getNumberFromFile()
+	fmt.Println(getNumber)
 }
