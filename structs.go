8f9a9cd0fb2cd90b153d5994ae659d2325f2a881
WannaMinPaing <paingwanna@gmail.com>
2024-07-21 15:50:26 +0630

    add custom type

--- structs.go
+++ structs.go
@@ -2,53 +2,15 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
-type user struct {
-	firstName  string
-	middleName string
-	lastName   string
-	createdAt  time.Time
+type str string
+
+func (text str) log() {
+	fmt.Println(text)
 }
 
 func main() {
-	userFirstName := getUserData("First Name")
-	userMiddleName := getUserData("Middle Name")
-	userLastName := getUserData("Last Name")
-
-	var appUser user
-
-	appUser = user{
-		firstName:  userFirstName,
-		middleName: userMiddleName,
-		lastName:   userLastName,
-		createdAt:  time.Now(),
-	}
-
-	// getUserDetails(appUser)
-	appUser.getUserDetailsTwo()
-	appUser.clearName()
-	appUser.getUserDetailsTwo()
-
-}
-
-func getUserDetails(u user) {
-	fmt.Println(u.firstName, u.middleName, u.lastName)
-}
-
-func (u *user) getUserDetailsTwo() {
-	fmt.Println(u.firstName, u.middleName, u.lastName)
-}
-
-func (u *user) clearName() {
-	u.firstName = ""
-	u.lastName = ""
-}
-
-func getUserData(promptText string) string {
-	fmt.Println(promptText)
-	var value string
-	fmt.Scan(&value)
-	return value
+	var name str = "Wanna"
+	name.log()
 }
